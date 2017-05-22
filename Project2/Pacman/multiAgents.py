4213a78b8934e91d4b21afd269e41b8e21f3cfdf
YiFan.Wu <jameswu1212@gmail.com>
2017-05-22 17:16:19 +0800

    update q1

--- Project2/Pacman/multiAgents.py
+++ Project2/Pacman/multiAgents.py
@@ -15,6 +15,7 @@
 from util import manhattanDistance
 from game import Directions
 import random, util
+import time, math
 
 from game import Agent
 
@@ -41,6 +42,13 @@ class ReflexAgent(Agent):
         # Collect legal moves and successor states
         legalMoves = gameState.getLegalActions()
 
+        # current pos
+        currentPos = gameState.getPacmanPosition();
+        # print 'Now: %s' % (str(currentPos))
+
+        # # Let Capsules be a kind of food
+        # gameState.getFood()[23][1] = True
+
         # Choose one of the best actions
         scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
         bestScore = max(scores)
@@ -48,7 +56,9 @@ class ReflexAgent(Agent):
         chosenIndex = random.choice(bestIndices) # Pick randomly among the best
 
         "Add more of your code here if you want to"
-
+        # pause for a while
+        # print 'next: %s' % (legalMoves[chosenIndex])
+        # raw_input()
         return legalMoves[chosenIndex]
 
     def evaluationFunction(self, currentGameState, action):
@@ -67,19 +77,109 @@ class ReflexAgent(Agent):
         to create a masterful evaluation function.
         """
         # Useful information you can extract from a GameState (pacman.py)
-        print 123
-        successorGameState = currentGameState.generatePacmanSuccessor(action)
-        newPos = successorGameState.getPacmanPosition()
-        newFood = successorGameState.getFood()
-        newGhostStates = successorGameState.getGhostStates()
-        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
-
-        print newFood
-
-
         "[Project 3] YOUR CODE HERE"
+        # Map Information: 9*25 here, including walls
+        mapWidth = 23
+        mapHeight = 7
+
+        # Successor Information
+        successorGameState = currentGameState.generatePacmanSuccessor(action)
+
+        # Position Information 
+        newPos = successorGameState.getPacmanPosition()
+        newX, newY = [newPos[0], newPos[1]]      
+
+        # Features and Weights: Ghost > Food > Capsule
+        def featureGhost(newX, newY, state):
+            # Ghost Information
+            newGhostStates = state.getGhostStates()
+            newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+            ghostPos = state.getGhostPosition(1)
+            ghostX, ghostY = [ghostPos[0], ghostPos[1]]
+            # featureScore
+            fScore = 0
+            if abs(newX-ghostX) == 0 and abs(newY-ghostY) == 0:
+                fScore = -30
+            elif abs(newX-ghostX) <= 1 and abs(newY-ghostY) <= 1:
+                fScore = -20
+            return fScore
+
+        def featureFood(newX, newY, state, action):
+
+            newFoodExist = state.hasFood(newX, newY)
+            fScore = 0
+            if newFoodExist == True:
+                fScore += 20
+            if action == "West":
+                dx = [-2, -1, 0, -2, -1, 0, -2, -1, 0]
+                dy = [1, 1, 1, 0, 0, 0, -1, -1, -1]
+                xStart, xEnd, yStart, yEnd = [1, newX+1, 1, 9]
+            elif action == "North":
+                dx = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
+                dy = [2, 2, 2, 1, 1, 1, 0, 0, 0]
+                xStart, xEnd, yStart, yEnd = [1, 24, newY, 9]
+            elif action == "East":
+                dx = [0, 1, 2, 0, 1, 2, 0, 1, 2]
+                dy = [1, 1, 1, 0, 0, 0, -1, -1, -1]
+                xStart, xEnd, yStart, yEnd = [newX, 24, 1, 9]
+            elif action == "South":
+                dx = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
+                dy = [0, 0, 0, -1, -1, -1, -2, -2, -2]
+                xStart, xEnd, yStart, yEnd = [1, 24, 1, newY+1]
+            elif action == "Stop":
+                return 0
+
+            for i in range(9):
+                countX = newX + dx[i]
+                countY = newY + dy[i]
+                if countX > 0 and countX < 25 and countY > 0 and countY < 9:
+                    if state.hasFood(countX, countY):
+                        fScore += 1
+            minValue = 100
+            for y in range(yStart, yEnd, 1):
+                for x in range(xStart, xEnd, 1):
+                    if x == newX and y == newY:
+                        continue
+                    if state.hasFood(x,y) and abs(newX-x)+abs(newY-y) < minValue:
+                        minValue = abs(newX-x) + abs(newY-y)
+            if minValue != 100:
+                fScore += math.floor((30-minValue)/2)
+
+            return fScore
+
+        def featureCapsule(newX, newY, state):
+            newCapsule = state.getCapsules()
+            fScore = 0
+            if len(newCapsule) > 0:
+                capsuleX, capsuleY = [newCapsule[0][0], newCapsule[0][1]]
+            else:
+                fScore = 0
+                return fScore
+
+            if newX == capsuleX and newY == capsuleY:
+                fScore += 30
+            elif abs(newX-capsuleX) <= 1 and abs(newY-capsuleY) <= 1:
+                fScore += 25
+            elif abs(newX-capsuleX) <= 2 and abs(newY-capsuleY) <= 2:
+                fScore += 20
+            elif abs(newX-capsuleX) <= 3 and abs(newY-capsuleY) <= 3:
+                fScore += 15
+
+            return fScore
+
+        w1 = 5
+        f1 = featureGhost(newX, newY, successorGameState)
+        w2 = 1
+        f2 = featureFood(newX, newY, currentGameState, action)
+        w3 = 4
+        f3 = featureCapsule(newX, newY, currentGameState)
+        evalScore = w1 * f1 + w2 * f2 + w3 * f3
+
+        # print '%s, (%d %d %d) => %d' % (action, f1, f2, f3, evalScore)
+        # information output string
+        # print 'next:(%d,%d); Food:%s; ghost:(%d,%d)' % (newX, newY, newFoodExist, ghostX, ghostY)
         
-        return successorGameState.getScore()
+        return evalScore
 
 def scoreEvaluationFunction(currentGameState):
     """
